71b02c4d27ba17d593099eeaf02db996d8067eb5
sonaliitp1 <SonaliS@itpreneurpune.com>
2026-01-09 16:30:51 +0530

    final Commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base :"React_Vite_CICD1.git/",
+  base: '/React_Vite_CICD1/',
 })
