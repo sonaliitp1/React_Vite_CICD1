c84d21baffb3aa040c0e660a94cf3c1354f72a00
sonaliitp1 <SonaliS@itpreneurpune.com>
2026-01-09 17:57:40 +0530

    Add text2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ function App() {
     <div style={{color:'red',backgroundColor:'aqua',textAlign:'center'}}>
       <h1>React + Vite GitHub Pages Deploy</h1>
       <p>This is my First Page</p>
+      <div style={{width:'200px',border:'2px solid blue',margin:'auto'}}>
+
+        <h2>Welcome to my CI CD Pipeline project</h2>
+        </div>
+      
       <p>This Pipeline will be done by Yogita and Sonali</p>
     </div>
   )
