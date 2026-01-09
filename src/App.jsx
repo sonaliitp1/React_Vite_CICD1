7282e896a69ffe6617869b7279fbb9d6789fdaec
sonaliitp1 <SonaliS@itpreneurpune.com>
2026-01-09 16:59:22 +0530

    Add text1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 function App() {
   return (
-    <div style={{color:'red',backgroundColor:'aqua'}}>
+    <div style={{color:'red',backgroundColor:'aqua',textAlign:'center'}}>
       <h1>React + Vite GitHub Pages Deploy</h1>
       <p>This is my First Page</p>
+      <p>This Pipeline will be done by Yogita and Sonali</p>
     </div>
   )
 }
