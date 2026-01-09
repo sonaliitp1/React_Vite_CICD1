55d41e7c83aa4e8248beb7eed9a0a9efc227aa7d
sonaliitp1 <SonaliS@itpreneurpune.com>
2026-01-09 16:45:41 +0530

    Add text

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div style={{'color':'red','backgroundColor:aqua'}}>
+    <div style={{color:'red',backgroundColor:'aqua'}}>
       <h1>React + Vite GitHub Pages Deploy</h1>
       <p>This is my First Page</p>
     </div>
